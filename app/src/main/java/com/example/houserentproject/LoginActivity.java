f7deae2f1a74198171ba90270a278ce1474c51ad
Shuvo Talukdar <shuvo6904@gmail.com>
2021-02-26 09:53:15 +0600

    Designing Register page and Login page

---
+++ app/src/main/java/com/example/houserentproject/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.houserentproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().setTitle("Login Form");
+    }
+
+    public void buttonRegister(View view) {
+
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+        finish();
+    }
+}
